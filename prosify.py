6a22e3c8884fc818adef8729203edbf687f3c84a
Brayton Hall <braytonhall@gmail.com>
2020-05-14 22:23:05 -0400

    added title

--- prosify.py
+++ prosify.py
@@ -2,13 +2,15 @@ import streamlit as st
 import markovify
 import numpy as np
 
+
 text = open("ulysses.txt", "r").read()
 
-# Build the model.
+# Build the model
 text_model = markovify.Text(text)
 
+st.title('Markov Prose Generator for Ulysses, by James Joyce')
 number = st.selectbox("Type in the number of Markov-generated sentences you'd like:",
-                             np.arange(1, 21, 1))
+                      np.arange(1, 21, 1))
 
 for i in range(number):
     st.write(text_model.make_sentence())
