4c05d2ea98932d14c64af88038b3479519093874
Brayton Hall <braytonhall@gmail.com>
2020-05-15 11:25:15 -0400

    changed remote and options to 100

--- prosify.py
+++ prosify.py
@@ -10,7 +10,7 @@ text_model = markovify.Text(text)
 
 st.title('Markov Prose Generator for Ulysses, by James Joyce')
 number = st.selectbox("Select the number of sentences you'd like:",
-                      np.arange(1, 21, 1))
+                      np.arange(1, 101, 1))
 
 for i in range(number):
     st.write(text_model.make_sentence())
